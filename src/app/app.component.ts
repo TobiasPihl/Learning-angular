467bd35748f1de29cc7cca5ba078adccb04bc49d
Tobias <tobpi009@student.liu.se>
2018-01-11 12:37:03 +0100

    template code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
 
+import { headerComponent } from './header/app.headerComponent';
+import { navComponent } from './nav/app.navComponent';
+import { contentAreaComponent } from './contentArea/app.contentAreaComponent';
+import { footerComponent } from './footer/app.footerComponent';
+
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './main.html',
+  styleUrls: ['./style.css']
 })
 export class AppComponent {
   title = 'app';
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
-
+import { headerComponent } from './header/app.headerComponent';
+import { navComponent } from './nav/app.navComponent';
+import { contentAreaComponent } from './contentArea/app.contentAreaComponent';
+import { footerComponent } from './footer/app.footerComponent'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, 
+    headerComponent,
+    navComponent,
+    contentAreaComponent,
+    footerComponent,
   ],
   imports: [
     BrowserModule
